87d495d4f746e691d790d0b62691434c7a2911e7
PritamBarua2006 <pritam_202400084@smit.smu.edu.in>
2026-07-25 12:16:08 +0530

    Configure MongoDB Atlas connection and update project

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,23 +1,24 @@
 console.log("db.js loaded");
 
-
 const mongoose = require("mongoose");
 
 async function connectDB() {
-
     try {
+        console.log("MONGO_URI:", process.env.MONGO_URI);
 
         await mongoose.connect(process.env.MONGO_URI);
 
         console.log("✅ MongoDB Connected");
 
     } catch (error) {
+    console.error("MongoDB connection failed:");
+    console.error(error);
+    console.error("Error name:", error.name);
+    console.error("Error message:", error.message);
+    console.error("Error cause:", error.cause);
 
-        console.log(error);
-        process.exit(1);
-
-    }
-
+    process.exit(1);
+}
 }
 
 module.exports = connectDB;
